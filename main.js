0517f0baf6ffc090450b05822f280be858d614d7
StacyGilbert7713 <63259897+StacyGilbert7713@users.noreply.github.com>
2020-08-18 22:10:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 let mainElement = document.createElement('main')
-    document.body.appendChild(mainElement)
+let bodyElement = document.querySelector('body')
+bodyElement.append(mainElement)
 
 let remove = document.createElement('button')
 let button = document.createTextNode('Do NOT click!!!')
 remove.append(button)
-mainElement.append(remove)
+bodyElement.append(remove)
 
 const cuteKitten = document.createElement('img')
 cuteKitten.src = "https://thecatsite.com/attachments/scottish-fold-kitten-jpg.261816/"
-cuteKitten.className = 'images'
+cuteKitten.className = 'image'
 mainElement.append(cuteKitten)
 cuteKitten.style.height = 300 + 'px'
 cuteKitten.style.width = 500 + 'px'
 
-const crazyCats = document.createElement('a')
+let crazyCats = document.createElement('a')
 crazyCats.href = 'https://www.youtube.com/watch?v=7aNgiSExiYI'
 crazyCats.classList = 'link'
 let videoText = document.createTextNode('Click here for More Kitties!')
@@ -22,5 +23,5 @@ mainElement.append(crazyCats)
 
 remove.addEventListener("click", function() {
     alert ('Bye-Bye! Toodles! Farewell! You clicked the Button, you lose the cuteness!!!!')
-    mainElement.remove()
+    bodyElement.remove()
 })
